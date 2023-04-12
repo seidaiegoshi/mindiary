60409a9f60d4871e08510323af130945cdc0844a
seidaiegoshi <egoshi.seidai@gmail.com>
2023-04-12 10:53:00 +0900

    googleログインとカレンダーイベントの表示まで

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Google Login Demo',
+      title: 'Mindiary',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   final GoogleSignIn _googleSignIn = GoogleSignIn(
+    clientId:
+        "153597970793-o59ipltl1memjvjmaap3l9la2q87rgbn.apps.googleusercontent.com",
     scopes: [
       'email',
       'https://www.googleapis.com/auth/calendar',
     ],
   );
   GoogleSignInAccount? _currentUser;
+  List<dynamic> _calendarEvents = [];
 
   @override
   void initState() {
@@ -53,11 +62,36 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<void> _handleSignOut() => _googleSignIn.disconnect();
 
+  Future<void> _getCalendarEvents() async {
+    if (_currentUser == null) return;
+
+    String authHeader = await _currentUser!.authHeaders
+        .then((headers) => headers['Authorization']!);
+
+    final response = await http.get(
+      Uri.parse('http://localhost/calendar/events'),
+      headers: {
+        'Authorization': authHeader,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final events = json.decode(response.body);
+      // Process and display the events
+      setState(() {
+        _calendarEvents = events;
+        // print(events);
+      });
+    } else {
+      print('Failed to fetch calendar events');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Google Login Demo'),
+        title: const Text('Mindiary'),
       ),
       body: Center(
         child: Column(
@@ -68,17 +102,40 @@ class _MyHomePageState extends State<MyHomePage> {
                 backgroundImage: NetworkImage(_currentUser!.photoUrl ?? ''),
               ),
               Text(_currentUser!.displayName ?? '',
-                  style: TextStyle(fontSize: 24)),
-              Text(_currentUser!.email, style: TextStyle(fontSize: 18)),
+                  style: const TextStyle(fontSize: 24)),
+              Text(_currentUser!.email, style: const TextStyle(fontSize: 18)),
               ElevatedButton(
-                child: Text('Logout'),
                 onPressed: _handleSignOut,
+                child: const Text('Logout'),
+              ),
+              ElevatedButton(
+                child: Text('Get Calendar Events'),
+                onPressed: _getCalendarEvents,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _calendarEvents.length,
+                  itemBuilder: (context, index) {
+                    final event = _calendarEvents[index];
+                    return ListTile(
+                      title: Text(event['summary']),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(event['start']),
+                          Text(event['description'] ??
+                              ''), // Add the description here
+                        ],
+                      ),
+                    );
+                  },
+                ),
               ),
             ] else ...[
-              Text('You are not logged in.'),
+              const Text('You are not logged in.'),
               ElevatedButton(
-                child: Text('Login with Google'),
                 onPressed: _handleSignIn,
+                child: const Text('Login with Google'),
               ),
             ]
           ],
